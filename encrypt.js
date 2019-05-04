c62b865d1c2600f99bfa7121be612737860ca0a5
rjagge <rjagge@163.com>
2019-05-04 22:18:14 +0800

    update_console_log

--- encrypt.js
+++ encrypt.js
@@ -2,12 +2,25 @@ const NodeRSA = require('node-rsa');
 const key = new NodeRSA({b: 512});
 
 const text = 'Hello RSA!';
+console.log('*****************encrypt*******************');
 const encrypted = key.encrypt(text, 'base64');
 console.log('encrypted: ', encrypted);
+console.log('*****************encrypt*******************');
+
+console.log('*******************************************');
+console.log('*****************decrypt*******************');
 const decrypted = key.decrypt(encrypted, 'utf8');
 console.log('decrypted: ', decrypted);
+console.log('*****************decrypt*******************');
 
+console.log('*******************************************');
+console.log('*****************sign**********************');
 const sign = key.sign(text,'base64');
 console.log('signed:', sign);
+console.log('*****************sign**********************');
+
+console.log('*******************************************');
+console.log('*****************verify**********************');
 const verify = key.verify(text,sign,'utf8','base64');
 console.log('verify:', verify);
+console.log('*****************verify**********************');
